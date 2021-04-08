31e0de293b029b49499fbc0ed61d214107ef818b
yundeveloper <yuniorbatista1113@gmail.com>
2021-04-08 08:05:10 -0400

    Updates

---
+++ precache-manifest.3e7385c2dac241963c4ad404de515e8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f601a3f35343201dc215211070fc27",
+    "url": "/playbook-ui-test/index.html"
+  },
+  {
+    "revision": "aa768fc397e61b3f2188",
+    "url": "/playbook-ui-test/static/css/2.b4e37009.chunk.css"
+  },
+  {
+    "revision": "3b758da4fc9d13a0f380",
+    "url": "/playbook-ui-test/static/css/main.41a5b900.chunk.css"
+  },
+  {
+    "revision": "aa768fc397e61b3f2188",
+    "url": "/playbook-ui-test/static/js/2.bd0b7057.chunk.js"
+  },
+  {
+    "revision": "3b758da4fc9d13a0f380",
+    "url": "/playbook-ui-test/static/js/main.d2347b17.chunk.js"
+  },
+  {
+    "revision": "739a90bd0362da2adfa1",
+    "url": "/playbook-ui-test/static/js/runtime~main.57f689fd.js"
+  }
+]);
